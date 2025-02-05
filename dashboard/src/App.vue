cd3a539176f53a76d23017b268f0104888768280
Farag-trycar <afarag@trycar.com>
2025-02-05 19:35:46 +0200

    feat/dash-api-call

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -5,5 +5,16 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue';
+
+onMounted(async ()=>{
+  let res = await fetch(import.meta.env.VITE_APP_API_URL,{
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',      
+    }
+  });
+  console.log(res);
+})
   //
 </script>

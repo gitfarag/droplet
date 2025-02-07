c2e4463c6d12065270eb7bb65560eaaf7fdffabe
Farag-trycar <afarag@trycar.com>
2025-02-07 05:57:12 +0200

    fix/seed-demo1

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -6,7 +6,7 @@ dotenv.config(); // Load environment variables from .env file
 module.exports = {
   development: {
     dialect: 'mysql',
-    host: process.env.MYSQL_HOST || 'localhost',
+    host: process.env.MYSQL_MIGRATION_HOST || 'localhost',
     port: process.env.MYSQL_PORT || 3306,
     username: process.env.MYSQL_USER || 'root',
     password: process.env.MYSQL_PASSWORD || 'password',

a6f887421158718a754e0a8fefc650cb7675fa9c
Farag-trycar <afarag@trycar.com>
2025-02-07 15:12:42 +0200

    feat/apiKey-seeder-1

--- backend/src/config/seeders/20250207125848-demo-api-key.js
+++ backend/src/config/seeders/20250207125848-demo-api-key.js
@@ -35,6 +35,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-    await queryInterface.bulkDelete('users', null, {});
+    await queryInterface.bulkDelete('ApiKeys', null, {});
   }
 };

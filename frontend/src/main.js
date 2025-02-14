43c27065b3a26c4c451a399d207df397f033ca27
Farag-trycar <afarag@trycar.com>
2025-02-14 22:33:07 +0200

    revamp/resume

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,24 @@
-import { createApp } from 'vue'
-import router from './router'
-import App from './App.vue'
-import './css/styles.css'
-import 'vue3-carousel/dist/carousel.css'
+/**
+ * Created by Ryan Balieiro on 08.23.2023
+ * App's entry point
+ */
+import './scss/style.scss'
+import {useData} from "./composables/data.js"
+import {useLanguage} from "./composables/language.js"
+import {useNavigation} from "./composables/navigation.js"
+import {createAppRouter} from "./router/router.js"
+import {createApp} from "vue"
+import App from './vue/core/App.vue'
 
-// intialize app
-createApp(App)
-    .use(router)
-    .mount('#app')
+const data = useData()
+
+data.fetchEssentials().then(r => {
+    const language = useLanguage()
+    language.init(data.getSettings()['supportedLanguages'])
+
+    const navigation = useNavigation()
+    navigation.init(data.getSections(), data.getCategories())
+
+    const router = createAppRouter()
+    createApp(App).use(router).mount('#app')
+})
